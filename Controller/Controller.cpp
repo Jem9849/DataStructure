1a1ffe18672bc3112330605382f2312deb2e1d88
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-31 12:11:49 -0700

    Added code to test out the timer even more.

--- Controller/Controller.cpp
+++ Controller/Controller.cpp
@@ -13,7 +13,18 @@ using namespace std;
 void Controller :: start()
 {
 	Timer codeTimer;
+	codeTimer.startTimer();
+	cout << "Look" << endl;
 	codeTimer.stopTimer();
+	codeTimer.displayInformation();
+	codeTimer.resetTimer();
+	codeTimer.startTimer();
+	for (int index = 0; index < 500; index++)
+	{
+		cout << "Index is " << index << "\n";
+	}
+	codeTimer.stopTimer();
+	codeTimer.displayInformation();
 }
 
 
