8c93e9eec86d52901cb3bf422603dd9f6cb40950
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-31 12:52:23 -0700

    Added files into a data folder that are about crimedata and added filecontroller stuff in controller. They read from files and things and stuff to do with files. I also added a crime.csv thingy which I assume is what is going to be read from or whatever file thingy.

---
+++ Controller/FileController.cpp
@@ -0,0 +1,50 @@
+/*
+ * FileController.cpp
+ *
+ *  Created on: Jan 31, 2018
+ *      Author: Jeremy
+ */
+
+#include "FileController.hpp"
+
+FileController :: FileController()
+{
+
+}
+
+vector<CrimeData> FileController :: readCrimeDataToVector(string filename)
+{
+	std :: vector<CrimeData> crimeVector;
+	string currentCSVLine;
+	int rowCount = 0;
+
+	ifstream dataFile(filename);
+
+	// If the file exists at that path.
+	if (dataFile.is_open())
+	{
+		// Keep reading until end of file/
+		while (!dataFile.eof())
+		{
+			// Grab each line from CSV seperated by carriage return character.
+			getline(dataFile, currentCSVLine, '\r');
+			// Exclude header row
+			if (rowCount != 0)
+			{
+				// Create a CrimeData instance from the line.
+				CrimeData row(currentCSVLine);
+				crimeVector.push_back(row);
+			}
+			rowCount++;
+		}
+		dataFile.close();
+	}
+	else
+	{
+		cerr << "NO FILE" << endl;
+	}
+
+	return crimeVector;
+}
+
+
