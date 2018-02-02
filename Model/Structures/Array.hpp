e1724a604b7403a1f704b1e993c7dc8b72c4daab
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-02 12:41:36 -0700

    Added some code for template that has operators, destructor, copy constructor, and methods. It isn't fully finished yet, but I wanted to commit since I didn't do so before.

---
+++ Model/Structures/Array.hpp
@@ -0,0 +1,109 @@
+/*
+ * Array.hpp
+ *
+ *  Created on: Feb 2, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_STRUCTURES_ARRAY_HPP_
+
+#include <assert.h> // Used for validating user supplied data.
+#include <iostream> // Used for tracing and debug statements.
+
+using namespace std; // Keyword access.
+
+#define MODEL_STRUCTURES_ARRAY_HPP_
+
+template <class Type>
+class Array
+{
+private:
+	Type * internalArray;
+	int size;
+
+public:
+	// Constructor.
+	Array<Type>(int size);
+
+	// Copy Constructor.
+	Array<Type>(const Array<Type> & toCopy);
+
+	// Destructor.
+	~Array<Type>();
+
+	// Operators.
+	Array<Type> & operator = (const Array<Type> & toReplace);
+	Type& operator [] (int index);
+	Type operator [] (int index) const;
+
+	// Methods.
+	int getSize() const;
+	Type getFromINdex(int index);
+	void setAtIndex(int index, Type data);
+
+
+};
+
+template <class Type>
+Array<Type> :: Array(int size)
+	{
+		assert(size > 0);
+		this-> size = size;
+
+		internalArray = new Type[size];
+	}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+	this-> size = toCopy.getSize();
+
+	// Build Data Structure
+	internalArray = new Type[size];
+
+	for(int index = 0; index < size; index++)
+	{
+		internalArray[index] = toCopy[index];
+	}
+}
+
+
+template <class Type>
+Array<Type> :: ~Array()
+{
+	delete [] internalArray;
+}
+
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+	if (&toAssign != this)
+	{
+		if (size != toAssign.getSize())
+		{
+			delete [] internalArray;
+			size = toAssign.getSize();
+			internalArray = new Type [size];
+		}
+
+
+		for (int index = 0; index < size; index++)
+		{
+			internalArray[index] = toAssign[index];
+		}
+	}
+
+	return *this;
+}
+
+template <class Type>
+Type & Array<Type> :: operator [] (int index)
+{
+	assert(index >= 0 && index < size);
+	return internalArray[index];
+}
+
+
+
+
+#endif /* MODEL_STRUCTURES_ARRAY_HPP_ */
