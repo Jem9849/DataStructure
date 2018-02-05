773180541b8a9e6fa97c20efd35ff0fbad9e518b
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-05 08:59:42 -0700

    Added a new folder called Nodes, and a new header file named Node.

---
+++ Model/Nodes/Node.hpp
@@ -0,0 +1,15 @@
+/*
+ * Node.hpp
+ *
+ *  Created on: Feb 5, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_NODE_NODE_HPP_
+#define MODEL_NODE_NODE_HPP_
+
+
+
+
+
+#endif /* MODEL_NODE_NODE_HPP_ */
