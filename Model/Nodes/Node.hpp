ccf7ab772e1be2d73218037eafac7a9b37ae6cec
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-05 09:03:42 -0700

    Added in the class and added the template thng. I also added in the public and protected sections. I put a call to the constructor in public and a bunch of other stuff, and I put something protected that has a type of TYpe.

--- Model/Nodes/Node.hpp
+++ Model/Nodes/Node.hpp
@@ -8,6 +8,18 @@
 #ifndef MODEL_NODE_NODE_HPP_
 #define MODEL_NODE_NODE_HPP_
 
+template <class Type>
+class Node {
+
+public:
+	Node();
+	Node(Type data);
+	void getData(Type data);
+	Type getData();
+protected:
+	Type data;
+
+};
 
 
 
