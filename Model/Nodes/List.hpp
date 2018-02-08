9f7c3963c3463c80904ca3ccef7083b145c3fb1e
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-08 12:54:42 -0700

    Wrote the code for the structure and helpers. Also the data member.

--- Model/Nodes/List.hpp
+++ Model/Nodes/List.hpp
@@ -8,7 +8,25 @@
 #ifndef MODEL_NODES_LIST_HPP_
 #define MODEL_NODES_LIST_HPP_
 
+#include <assert.h>
+#include "../../Nodes/LinearNode.hpp"
 
+template <class Type>
+class List
+{
+public:
+	// Structure
+	virtual void add(Type item) = 0;
+	virtual void addAtIndex(int index, Type item);
+	virtual Type remove(int index) = 0;
+	virtual Type getFromIndex(int index) = 0;
+	// Helper
+	virtual int getSize() const = 0;
+	virtual LinearNode<Type> * getFront() = 0;
+	virtual LinearNode<Type> * getEnd() = 0;
+protected:
+	int size;
+};
 
 
 
