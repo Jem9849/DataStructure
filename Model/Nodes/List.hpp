c0ba6706d39a3a9602fa1372a39b04defbabd3f8
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-08 12:45:13 -0700

    Changed around the plan and added for list and also added a list header.

---
+++ Model/Nodes/List.hpp
@@ -0,0 +1,15 @@
+/*
+ * List.hpp
+ *
+ *  Created on: Feb 8, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_NODES_LIST_HPP_
+#define MODEL_NODES_LIST_HPP_
+
+
+
+
+
+#endif /* MODEL_NODES_LIST_HPP_ */
