f5a52996c65121bdcb815808340e07c77af4ca40
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-14 12:17:54 -0700

    Implemented the readDataToList method and fixed an import.

--- Controller/FileController.hpp
+++ Controller/FileController.hpp
@@ -14,7 +14,7 @@
 #include <iostream>
 #include <vector>
 #include <sstream> // Treats strings as a stream.
-#include LinkedList.hpp
+#include "LinkedList.hpp"
 
 using namespace std;
 
@@ -26,6 +26,44 @@ public:
 	static LinkedList<CrimeData> readDataToList(string filename);
 };
 
+LinkedList<CrimeData> FileController :: readDataToList(string filename)
+{
+	LinkedList<CrimeData> crimes;
+
+	string currentCSVLine;
+	int rowCount = 0;
+
+	ifstream dataFile(fileName);
+
+	// If the file exists at path.
+	if (dataFile.is_open())
+	{
+		// Read till at end.
+		while (!dataFile.eof())
+		{
+			// Grab each line separated by carriage return.
+			getline(dataFile, currentCSVLine, '\r');
+			// Exclude header row.
+			if (rowCount != 0)
+			{
+				// Create CrimeData instance. Exclude blank line.
+				if (currentCSVLine.length() != 0)
+				{
+					CrimeData row(currentCSVLine);
+					crimes.add(row);
+				}
+			}
+			rowCount++;
+		}
+		dataFile.close();
+	}
+	else
+	{
+		cerr << "No File." << endl;
+	}
+
+	return crimes;
+}
 
 
 #endif /* FileController_hpp */
