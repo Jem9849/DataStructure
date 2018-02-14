a07ab60bdc6de866286c3957cf6351434dbe78d4
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-14 11:45:56 -0700

    Added a new header structure called LinkList and added some starter code for now.

---
+++ Model/Structures/LinkedList.hpp
@@ -0,0 +1,22 @@
+/*
+ * LinkedList.hpp
+ *
+ *  Created on: Feb 14, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_STRUCTURES_LINKEDLIST_HPP_
+#define MODEL_STRUCTURES_LINKEDLIST_HPP_
+#include <iostream>
+#include "List.hpp"
+
+using namespace std;
+
+class LinkedList
+{
+
+};
+
+
+
+#endif /* MODEL_STRUCTURES_LINKEDLIST_HPP_ */
