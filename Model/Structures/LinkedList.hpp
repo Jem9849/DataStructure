641a399c5d648ef73ff9c04c47a6dc58f270f5b5
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-14 11:50:44 -0700

    Added the constructor, destructor, helper methods, and structure methods.

--- Model/Structures/LinkedList.hpp
+++ Model/Structures/LinkedList.hpp
@@ -15,6 +15,23 @@ using namespace std;
 template <class Type>
 class LinkedList : public List<Type>
 {
+public:
+	// Constructors
+	LinkedList();
+	// Destructor
+	virtual ~LinkedList(); // Can be overridden when virtual.
+	// Helper Methods
+	int getSize() const;
+	LinearNode<Type> * getFront();
+	LinearNode<Type> * getEnd();
+
+	// Structure Methods
+	void add(Type item);
+	void addAtIndex(int index, Type item);
+	Type getFromIndex(int index);
+	Type remove(int index);
+	// Type setAtIndex(int index, Type item);
+	// bool contains(Type item);
 protected:
 	LinearNode<Type> * front;
 	LinearNode<Type> * end;
