4de1502590bdabd7a0a7c786b2e7f2fcc1ba6895
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-22 11:57:49 -0700

    Added in the template for stack header which I just made, and I also added the include statement. I then added the public section and then added the constructor and destructor declaration.

---
+++ Model/Structures/Linear/Stack.hpp
@@ -0,0 +1,25 @@
+/*
+ * Stack.hpp
+ *
+ *  Created on: Feb 22, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_STRUCTURES_LINEAR_STACK_HPP_
+#define MODEL_STRUCTURES_LINEAR_STACK_HPP_
+
+#include "LinkedList.hpp"
+
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+public:
+	Stack();
+	~stack();
+};
+
+
+
+
+
+#endif /* MODEL_STRUCTURES_LINEAR_STACK_HPP_ */
