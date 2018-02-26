2363eeee978c63c632dc684c94c7d6f22ea26b3f
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-26 11:55:08 -0700

    Added a new file in nodes called DoubleNode, and added the basic parts of it.

---
+++ Model/Nodes/DoubleNode.hpp
@@ -0,0 +1,22 @@
+/*
+ * DoubleNode.hpp
+ *
+ *  Created on: Feb 26, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_NODES_DOUBLENODE_HPP_
+#define MODEL_NODES_DOUBLENODE_HPP_
+
+#include "Node.hpp"
+
+template <class Type>
+class DoubleNode : public Node<Type>
+{
+
+};
+
+
+
+
+#endif /* MODEL_NODES_DOUBLENODE_HPP_ */
