83aa718fe6dd84d07fa85ddf55a6f0108c5a8ae8
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-02-26 12:17:30 -0700

    Added two new files for testing stacks and queues and added code that starts off the header file.

---
+++ Testers/TestingStacksAndQueues.hpp
@@ -0,0 +1,27 @@
+/*
+ * TestingStacksAndQueues.hpp
+ *
+ *  Created on: Feb 26, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef TESTERS_TESTINGSTACKSANDQUEUES_HPP_
+#define TESTERS_TESTINGSTACKSANDQUEUES_HPP_
+#include <iostream>
+
+#include "../Controller/Tools/Timer.cpp"
+#include "../Controller/FileController.cpp"
+#include "../Model/Structures/Linear/Queue.hpp"
+#include "../Model/Structures/Linear/Stack.hpp"
+#include "../Model/Structures/Linear/LinkedList.hpp"
+#include "../Model/Structures/Linear/List.hpp"
+
+class TestingStacksAndQueues
+{
+
+};
+
+
+
+
+#endif /* TESTERS_TESTINGSTACKSANDQUEUES_HPP_ */
