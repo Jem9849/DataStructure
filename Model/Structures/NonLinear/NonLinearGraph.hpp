18d5dd226637c3fc4a4843ad950a6c345a9f634a
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-19 12:29:42 -0600

    Added a new plan. I also added a nonlinear folder, and a header that has a template of a nonlineargraph.

---
+++ Model/Structures/NonLinear/NonLinearGraph.hpp
@@ -0,0 +1,66 @@
+/*
+ * NonLinearGraph.hpp
+ *
+ *  Created on: Mar 19, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_STRUCTURES_NONLINEAR_NONLINEARGRAPH_HPP_
+#define MODEL_STRUCTURES_NONLINEAR_NONLINEARGRAPH_HPP_
+
+#include <iostream>
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+
+template<class Type>
+class Graph
+{
+private:
+	static const int MAXIMUM = 20;
+	bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+	int weightCostMatrix [MAXIMUM][MAXIMUM];
+	Type graphData[MAXIMUM];
+
+	int vertexCount;
+
+	void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markeVertices[]);
+public:
+	Graph();
+	//Graph operations
+
+	void addVertex(const Type& value);
+
+	//Connect vertices
+	void addEdge(int source, int target);
+	void addEdgeUndirected(int source, int target);
+	void addEdgeCost(int source, int target, int cost);
+
+	//Disconnect vertices
+	void removeEdge(int source, int target);
+	void removeEdgeUndirected(int soruce, int target);
+	void removeEdgeCost(int source, int target);
+
+	//Accessors
+	Type& operator [] (int vertex);
+	Type operator [] (int vertex) const;
+	int size() const;
+
+	//Check connections
+	bool hasUndirectedConnection(int source, int target) const;
+	bool areConnected(int source, int target) const;
+	std::set<int> neighbors(int vertex) const;
+
+	//Traversals
+	void depthFirstTraversal(Graph<Type> & graph, int vertex);
+	void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+	int costTraversal(Graph<Type> & graph, int vertex);
+};
+
+
+
+
+
+#endif /* MODEL_STRUCTURES_NONLINEAR_NONLINEARGRAPH_HPP_ */
