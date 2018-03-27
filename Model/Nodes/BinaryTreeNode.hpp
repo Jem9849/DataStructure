7551eb53be9dc98cdccf4b91b0ae9fcf1e8adb62
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-27 12:08:04 -0600

    Added new plans, and added a new file for binaryTreeNode.

---
+++ Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,15 @@
+/*
+ * BinaryTreeNode.hpp
+ *
+ *  Created on: Mar 27, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_NODES_BINARYTREENODE_HPP_
+#define MODEL_NODES_BINARYTREENODE_HPP_
+
+
+
+
+
+#endif /* MODEL_NODES_BINARYTREENODE_HPP_ */
