a5e4b771ed82b10b2803c34ad876218b54e97607
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-27 12:41:40 -0600

    Added code for making the template class of BinaryTreeNode.

--- Model/Nodes/BinaryTreeNode.hpp
+++ Model/Nodes/BinaryTreeNode.hpp
@@ -8,6 +8,79 @@
 #ifndef MODEL_NODES_BINARYTREENODE_HPP_
 #define MODEL_NODES_BINARYTREENODE_HPP_
 
+#include "Node.hpp"
+
+template <class Type>
+class BinaryTreeNode : public Node<Type>
+{
+private:
+	BinaryTreeNode<Type> * root;
+	BinaryTreeNode<Type> * right;
+	BinaryTreeNode<Type> * left;
+public:
+	BinaryTreeNode();
+	BinaryTreeNode(Type data);
+
+	BinaryTreeNode<Type> * getRootNode();
+	BinaryTreeNode<Type> * getRightNode();
+	BinaryTreeNode<Type> * getLeftNode();
+
+	void setRootNode(BinaryTreeNode<Type> * root);
+	void setLeftNode(BinaryTreeNode<Type> * right);
+	void setRightNode(BinaryTreeNode<Type> * left);
+};
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+	root = nullptr;
+	left = nullptr;
+	right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
+{
+	root = nullptr;
+	left = nullptr;
+	right = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRootNode()
+{
+	return this -> root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightNode()
+{
+	return this -> right;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftNode()
+{
+	return this -> left;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRootNode(BinaryTreeNode<Type> * root)
+{
+	this -> root = root;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightNode(BinaryTreeNode<Type> * right)
+{
+	this -> right = right;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftNode(BinaryTreeNode<Type> * left)
+{
+	this -> left = left;
+}
 
 
 
