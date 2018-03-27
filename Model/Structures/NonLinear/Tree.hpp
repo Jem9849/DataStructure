1b9d0defc45a13e69a741b70c7a58973e0e5f529
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-03-27 13:36:39 -0600

    Added the declaration for methods and traversals in the tree template.

--- Model/Structures/NonLinear/Tree.hpp
+++ Model/Structures/NonLinear/Tree.hpp
@@ -10,6 +10,29 @@
 
 #include "../../Model/Nodes/BinaryTreeNode.hpp"
 
+template <class Type>
+class Tree
+{
+	protected:
+	BinaryTreeNode<Type> * root;
+
+public:
+	//Informational Methods
+	virtual int getSize() = 0;
+	virtual int getHeight() = 0;
+	virtual bool isComplete() = 0;
+	virtual bool isBalanced() = 0;
+
+	//Data Methods
+	virtual void insert(Type data) = 0;
+	virtual bool contains(Type data) = 0;
+	virtual void remove(Type data) = 0;
+
+	//Traversals
+	virtual void inOrderTraversal() = 0;
+	virtual void preOrderTraversal() = 0;
+	virtual void postOrderTraversal() = 0;
+};
 
 
 
