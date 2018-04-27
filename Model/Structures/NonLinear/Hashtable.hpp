d0a1fc39510168cd7f5e4446d871e60696eb4064
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-04-27 12:20:49 -0600

    New header file called hashtable.

---
+++ Model/Structures/NonLinear/Hashtable.hpp
@@ -0,0 +1,15 @@
+/*
+ * Hashtable.hpp
+ *
+ *  Created on: Apr 27, 2018
+ *      Author: Jeremy
+ */
+
+#ifndef MODEL_STRUCTURES_NONLINEAR_HASHTABLE_HPP_
+#define MODEL_STRUCTURES_NONLINEAR_HASHTABLE_HPP_
+
+
+
+
+
+#endif /* MODEL_STRUCTURES_NONLINEAR_HASHTABLE_HPP_ */
