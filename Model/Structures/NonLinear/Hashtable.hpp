fd23567849779f19d82f5fae16fb52f00d8527aa
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-04-27 12:27:48 -0600

    Added declarations and some stubs.

--- Model/Structures/NonLinear/Hashtable.hpp
+++ Model/Structures/NonLinear/Hashtable.hpp
@@ -8,7 +8,50 @@
 #ifndef MODEL_STRUCTURES_NONLINEAR_HASHTABLE_HPP_
 #define MODEL_STRUCTURES_NONLINEAR_HASHTABLE_HPP_
 
+#include "../Nodes/HashNode.hpp"
 
+template <class Type>
+class Hashtable
+{
+private:
+	HashNode<Type> ** internalStorage;
+	long size;
+	long capacity;
+	double loadFactor;
+
+	long getNextPrime();
+	bool isPrime(long current);
+	void resize();
+	long findPosition(HashNode<Type> * insertNode);
+	long handleCollision(HashNode<Type> * insertedNode, long index);
+public:
+	Hashtable();
+	~Hashtable();
+
+	void insert(Type data);
+	long getSize();
+};
+
+template <class Type>
+Hashtable<Type> :: Hashtable()
+{
+	capacity = 101;
+	internalStorage = new HashNode<Type> * [capacity];
+	size = 0;
+	loadFactor = .80;
+}
+
+template <class Type>
+Hashtable<Type> :: ~Hashtable()
+{
+
+}
+
+template <class Type>
+bool Hashtable<Type> :: isPrime(long current)
+{
+
+}
 
 
 
