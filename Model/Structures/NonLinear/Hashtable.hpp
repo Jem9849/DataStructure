e91f91056abaf93a9119782767a72e1a45154a5f
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-05-07 11:51:31 -0600

    Added code for the resize implementation.

--- Model/Structures/NonLinear/Hashtable.hpp
+++ Model/Structures/NonLinear/Hashtable.hpp
@@ -80,7 +80,36 @@ long Hashtable<Type> :: getNextPrime()
 template <class Type>
 void Hashtable<Type> :: resize()
 {
-
+    long updatedCapacity = getNextPrime();
+    HashNode<Type> * * tempStorage = new HashNode<Type> * [updatedCapacity];
+    
+    std :: fill_n(tempStorage, updatedCapacity, nullptr);
+    
+    long oldCapacity = this -> capacity;
+    this -> capacity = updatedCapacity;
+    
+    for (long index = 0; index < oldCapacity; index++)
+    {
+        if (internalStorage[index] != nullptr)
+        {
+            HashNode<Type> * temp = internalStorage[index];
+            
+            long position = findPosition(temp);
+            if (tempStorage[position] == nullptr)
+            {
+                tempStorage[position] = temp;
+            }
+            else
+            {
+                long updatedPosition = handleCollision(position);
+                if (updatedPosition != -1)
+                {
+                    tempStorage[updatedPosition] = temp;
+                }
+            }
+        }
+    }
+    internalStorage = tempStorage;
 }
 
 template <class Type>
